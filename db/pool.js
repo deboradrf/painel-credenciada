fd5b42280e88a8798875da57c5aee8854bece7d2
deboradrf <deborapfonseca@yahoo.com>
2026-02-03 13:46:13 -0300

    configura pool para vps

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,32 @@
-const { Pool } = require("pg");
+/*const { Pool } = require("pg");
 
 const pool = new Pool({
     user: "postgres",
     host: "localhost",
     database: "cadastro_funcionarios",
     password: "salubrita",
+    port: 5432
+});
+
+// DEBUG
+pool.on("connect", () => {
+    console.log("🟢 Conectado ao PostgreSQL");
+});
+
+pool.on("error", (err) => {
+    console.error("🔴 Erro inesperado no PostgreSQL", err);
+    process.exit(1);
+});
+
+module.exports = pool;*/
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+    user: "adm_salubrita",
+    host: "localhost",
+    database: "painel_salubrita",
+    password: "salub@2026",
     port: 5432
 });
 
